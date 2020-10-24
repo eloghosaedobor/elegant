a78141a11eb96c079c85673939684d35f0e1302f
Eloghosa Edobor <eloghosaedobor3@gmail.com>
2020-10-24 23:25:36 +0100

    Updates

---
+++ precache-manifest.b666a79a31cf28506f3bb9235cfbbe09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf0b24014b7ab028cbc7e2d97d04850",
+    "url": "/elegant/index.html"
+  },
+  {
+    "revision": "4d2d7837d4414e0850b3",
+    "url": "/elegant/static/css/main.ead691be.chunk.css"
+  },
+  {
+    "revision": "b59de592b0ed9955aa56",
+    "url": "/elegant/static/js/2.17ccef7d.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/elegant/static/js/2.17ccef7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2d7837d4414e0850b3",
+    "url": "/elegant/static/js/main.9da2c4af.chunk.js"
+  },
+  {
+    "revision": "29e3094d7e160cdf914d",
+    "url": "/elegant/static/js/runtime-main.abfc2589.js"
+  }
+]);
