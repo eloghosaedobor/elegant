bfe37367034751d985f744b8fdcf49208e0d563a
Eloghosa Edobor <eloghosaedobor3@gmail.com>
2020-10-27 17:20:43 +0100

    Updates

---
+++ precache-manifest.f00e6af69cc848021ba168eee62b2eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c084ba11c8e0921076ff294f984d3feb",
+    "url": "/elegant/index.html"
+  },
+  {
+    "revision": "8fc93385ab728fb22884",
+    "url": "/elegant/static/css/main.7ac61e20.chunk.css"
+  },
+  {
+    "revision": "abe0772a5b8cec86849f",
+    "url": "/elegant/static/js/2.f3f9ed5b.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/elegant/static/js/2.f3f9ed5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc93385ab728fb22884",
+    "url": "/elegant/static/js/main.58650312.chunk.js"
+  },
+  {
+    "revision": "29e3094d7e160cdf914d",
+    "url": "/elegant/static/js/runtime-main.abfc2589.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elegant/precache-manifest.86de15d5471cd42fa795b7654de56f4a.js"
+  "/elegant/precache-manifest.f00e6af69cc848021ba168eee62b2eb5.js"
 );
 
 self.addEventListener('message', (event) => {
